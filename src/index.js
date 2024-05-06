d3772f0fcc8f67c3a9729afea92252d92e1be5e6
Eduardo Lopes <montalverne.edu@gmail.com>
2024-05-06 20:04:29 -0300

    work in progress (wip): Added images of the project to the assets folder. Developement and styling of the navbar component and index files.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById("root")); // How to update this, once it is deprecated?
+ReactDOM.render(<App />, document.getElementById("root")); // How to update this .render(), once it is deprecated?
